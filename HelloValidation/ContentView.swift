ca479796774a20c630666223a6ea4bd9524ad7fd
superkookai <weerawut@live.com>
2025-03-04 08:59:55 +0700

    Initial Commit

---
+++ HelloValidation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloValidation
+//
+//  Created by Weerawut Chaiyasomboon on 04/03/2568.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloValidation/HelloValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloValidationApp.swift
+//  HelloValidation
+//
+//  Created by Weerawut Chaiyasomboon on 04/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct HelloValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
